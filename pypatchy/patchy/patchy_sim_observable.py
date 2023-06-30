070f21c1cb96eeeb2f01e114a35b46034d2396ca
Josh Evans <joshuaevanslowell@gmail.com>
2023-06-30 16:40:35 -0700

    fixing observable constructor again bc i didn't doit right the first time

--- pypatchy/patchy/patchy_sim_observable.py
+++ pypatchy/patchy/patchy_sim_observable.py
@@ -23,7 +23,7 @@ class PatchySimObservable:
         self.start_observe_stepnum = kwargs["start_from"] if "start_from" in kwargs else 0
         self.stop_observe_stepnum = kwargs["stop_at"] if "stop_at" in kwargs else None
         self.only_write_last = kwargs["only_last"] if "only_last" in kwargs else False
-        self.update_name_with_time = kwargs["update_name_with_time"] if "update_name_with_name" else False
+        self.update_name_with_time = kwargs["update_name_with_time"] if "update_name_with_name" in kwargs else False
 
         self.cols = kwargs["cols"]  # abandon hope all ye who enter here
 
