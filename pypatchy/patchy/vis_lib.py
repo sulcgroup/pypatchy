97d4cc4bc23f7d811e0296f88b859c3232b87c33
Josh Evans <joshuaevanslowell@gmail.com>
2023-09-05 12:45:27 -0700

    fixed( hopefully) color computation in  vis lib

--- pypatchy/patchy/vis_lib.py
+++ pypatchy/patchy/vis_lib.py
@@ -172,7 +172,7 @@ def get_particle_color(ptypeidx: int):
     returns an rgb color consistant with the usage in polycubes
     """
     hue = ptypeidx * 137.508
-    return matplotlib.colors.hsv_to_rgb((hue / 360, .5, .5))
+    return matplotlib.colors.hsv_to_rgb(((hue % 360) / 360, .5, .5))
 
 
 def show_clusters(e: PatchySimulationEnsemble,
